af973523f38571d5ebb38eb485ce56edcce8f177
Fred <ricardofredt@hotmail.com>
2021-11-11 21:16:26 -0300

    added events and responses

--- socket-io-server/README.md
+++ socket-io-server/README.md
@@ -1,1 +1,50 @@
 npm run start
+
+### eventos emitidos pelo server:
+
+-   inicializa array de salas e array de users
+    getInfoOnConnectRes
+
+-   atualiza array de users
+    usersUpdate
+
+-   sala criada
+    roomsUpdate
+
+-   entrar na sala
+    joinRoomRes
+
+-   sair da sala
+    leaveRoomRes
+
+-   jogada
+    moveRes
+
+-   desconectar
+    disconnect
+
+### eventos recebidos pelo server:
+
+inicializa array de salas e array of users
+
+-   getInfoOnConnect
+
+registrar novo usuário
+
+-   registerUser
+
+criar sala
+
+-   createRoom
+
+entrar na sala
+
+-   joinRoom
+
+sair da sala
+
+-   leaveRoom
+
+fazer jogada
+
+-   move
--- socket-io-server/app.js
+++ socket-io-server/app.js
@@ -5,6 +5,7 @@ const io = require("socket.io")(3000, {
 		origin: ["http://127.0.0.1:6969", "https://admin.socket.io"],
 	},
 })
+
 //create room adapter
 // const adapter = new io.adapter.rooms.Redis({
 // 	host: "	redis",
@@ -13,25 +14,59 @@ const io = require("socket.io")(3000, {
 // 	password: "",
 // })
 
-//testar
-
+//
+let rooms = []
+let users = []
 io.on("connection", (socket) => {
-	console.log(socket.id)
-	//io that receives cell data from the client
-	socket.on("cellClick", (data) => {
-		console.log(data)
-		io.emit("cellClick", data)
+	//registrar novo usuário
+	socket.on("registerUser", (payload) => {
+		users.push({ userName: payload.userName, userId: socket.id })
+		console.log("registrando novo usuário: ", payload.userName)
+		io.emit("usersUpdate", users)
 	})
-	socket.on("join", (data) => {
-		socket.join(data)
-		console.log("conectando user à sala: ", data.room)
+	// inicializar salas e users
+	socket.on("getInfoOnConnect", () => {
+		io.emit("getInfoOnConnectRes", { rooms, users })
+		console.log("mandando infos: ", users)
 	})
-	//emit all rooms
-	socket.on("getRooms", () => {
-		io.emit("getRooms", io.sockets.adapter.rooms) // não está retornando as salas
+	// criar sala
+	socket.on("createRoom", (roomName) => {
+		if (!rooms.includes(roomName)) {
+			rooms.push(roomName)
+			socket.join(roomName)
+			io.emit("joinRoomRes", roomName)
+			io.emit("roomsUpdate", rooms)
+		}
 	})
-	socket.on("teste", () => {
-		io.emit("testeRes", "testado")
+	// entrar na sala
+	socket.on("joinRoom", (roomName) => {
+		if (rooms.includes(roomName)) {
+			socket.join(roomName)
+			io.emit("joinRoomRes", roomName)
+		} else {
+			socket.emit("joinRoomRes", {
+				success: false,
+				message: "Room does not exist",
+			})
+		}
+	})
+	// sair da sala
+	socket.on("leaveRoom", (roomName) => {
+		if (rooms.includes(roomName)) {
+			socket.leave(roomName)
+			socket.emit("leaveRoomRes", "Left room")
+		}
+	})
+	//
+	// fazer jogada (manda jogada para sala informada no data)
+	socket.on("move", (data) => {
+		io.broadcast
+			.to(data.room)
+			.emit("moveRes", { message: `usuário ${socket.id}` })
+	})
+	socket.on("disconnect", () => {
+		users = users.filter((user) => user.userId !== socket.id)
+		io.emit("usersUpdate", users)
 	})
 })
 
@@ -40,9 +75,11 @@ io.on("connection", (socket) => {
 // })
 
 // io that disconnects
-io.on("disconnect", () => {
-	console.log("disconnected")
-})
+// socket.on("disconnect", () => {
+// 	console.log("user disconnected")
+// })
+
+// remove user from users array when disconnected
 
 //create rooms for each pair of users
 
